cfade3f964c4f8b0b48c14f940ec05c8d711abab
Jorge Beserra <jorgebeserra@gmail.com>
2021-01-02 18:19:01 -0300

    Translate pt_BR
    Thanks for this package

---
+++ src/Resources/lang/pt_BR/app.php
@@ -0,0 +1,59 @@
+<?php
+
+return [
+    'admin' => [
+        'system'            => [
+            'pwa'                                   => 'PWA',
+            'btn-save'                              => 'Salvar',
+            'name'                                  => 'Nome',
+            'topic'                                 => 'Topico',
+            'media'                                 => 'Media',
+            'seo-author'                            => 'Autor',
+            'settings'                              => 'Configurações',
+            'general'                               => 'Geral',
+            'short-name'                            => 'Apelido',
+            'theme-color'                           => 'Cor Tema',
+            'add_in_pwa'                            => 'Adicionar ao PWA',
+            'messagingId'                           => 'Id Mensagens',
+            'api-key'                               => 'Chave API servidor',
+            'enable_slider'                         => 'Habilitar Slider',
+            'background-color'                      => 'Cor de Fundo',
+            'small'                                 => 'App Icone (48x48)',
+            'medium'                                => 'App Icone (96x96)',
+            'push-notification'                     => 'Notificações por Push',
+            'large'                                 => 'App Icone (144x144)',
+            'extra-large'                           => 'App Icone (196x196)',
+            'pwa_full_name'                         => 'Página Web Progressiva',
+            'enable_new'                            => 'Habilitar novos produtos',
+            'enable_featured'                       => 'Habilitar destaques de produtos',
+            'enable_categories_home_page_listing'   => 'Habilitar listagem da página inicial das categorias',
+            'redirect_to_pwa_if_mobile'             => 'Redirecionar o usuário para PWA se estiver usando um dispositivo móvel',
+        ],
+
+        'layouts'           => [
+            'index'             => 'PWA Layout',
+            'push-notification' => 'Notificações Push'
+        ],
+
+        'push-notification' => [
+            'icon'                  => 'Icone',
+            'label-title'           => 'Titulo',
+            'target-url'            => 'Target URL',
+            'description'           => 'Descrição',
+            'notification'          => 'Notificação',
+            'title-create'          => 'Adicionar Notificação',
+            'title-edit'            => 'Notificação de modificação',
+            'edit-notification'     => 'Editar Notificação Push',
+            'title'                 => 'Lista de notificações Push',
+            'create-notification'   => 'Criar Notificação Push',
+            'success-notification'  => 'Sucesso: Notificação push enviada com sucesso.'
+        ],
+
+        'datagrid'          => [
+            'id'            => 'Cód',
+            'title'         => 'Titulo',
+            'target-url'    => 'URL alvo',
+            'description'   => 'Descrição',
+        ]
+    ]
+];
