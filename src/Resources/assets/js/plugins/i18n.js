b3f699b1259097f1db843864c2bedeeee00d557c
Jorge Beserra <jorgebeserra@gmail.com>
2021-01-02 18:28:25 -0300

    Added to the VUE pt_BR option

--- src/Resources/assets/js/plugins/i18n.js
+++ src/Resources/assets/js/plugins/i18n.js
@@ -1,10 +1,12 @@
 import Vue     from 'vue';
 import VueI18n from 'vue-i18n';
 import en      from '../lang/en.json'
+import pt_BR      from '../lang/pt_BR.json'
 Vue.use(VueI18n);
 
 const messages = {
-    'en': en
+    'en': en,
+    'pt_BR': pt_BR
 };
 
 export default new VueI18n({
