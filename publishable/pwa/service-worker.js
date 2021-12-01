6f692bbe492868aabbfcc2a7e17a7817de68bfef
meraj quraishi <meraj.quraishi459@webkul.com>
2021-12-01 12:41:25 +0530

    fixed minor issue

--- publishable/pwa/service-worker.js
+++ publishable/pwa/service-worker.js
@@ -191,6 +191,11 @@ function formFilter(response) {
     });
 }
 
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries are not available in the service worker.
+importScripts('http://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('http://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+
 const firebaseConfig = {
   apiKey: "{{core()->getConfigData('pwa.settings.push-notification.web-api-key')}}",
   authDomain: "{{core()->getConfigData('pwa.settings.push-notification.auth-domain')}}",
